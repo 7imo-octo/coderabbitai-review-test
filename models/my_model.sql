fb6f5f22ba6ccab58280946971ee2fb1e9743cee
Timo Kraus <timo.kraus@octopusenergy.de>
2024-09-24 17:39:55 +0200

    Update my_model.sql

--- models/my_model.sql
+++ models/my_model.sql
@@ -3,9 +3,9 @@ WITH customer_data AS (
     SELECT * FROM customers
 )
 SELECT
-    ´customerName´ as customer_name
+    `customerName` as customer_name
     CAST(fav_number, INT) as fav_number,
     "favProduct" as fav_product
-    ORDER BY fav_number    
 FROM
-    customer_data;
+    customer_data
+ORDER BY fav_number;    
