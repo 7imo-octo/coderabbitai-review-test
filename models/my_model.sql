13cb2b98b67b75fb37e06b13d6b57a68cc99577c
Timo Kraus <timo.kraus@octopusenergy.de>
2024-09-24 17:30:00 +0200

    Update my_model.sql
    add more sql

--- models/my_model.sql
+++ models/my_model.sql
@@ -3,6 +3,9 @@ WITH customer_data AS (
     SELECT * FROM customers
 )
 SELECT
-    AVG(age) as average_age
+    ´customerName´ as customer_name
+    CAST(fav_number, INT) as fav_number,
+    "favProduct" as fav_product
+    ORDER BY fav_number    
 FROM
     customer_data;
